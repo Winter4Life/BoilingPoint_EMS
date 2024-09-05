03c8cea413007ca2ace029de409e9ca817f0d6ac
Winter4Life <taylork1201@yahoo.com>
2024-09-05 15:19:50 -0700

    Created menu display
    Checks if user typed in either of the correct options

--- BP_Database.py
+++ BP_Database.py
@@ -1,5 +1,7 @@
 # Employee Management System using Python
 
+from os import system
+import sys
 # Importing mySQL
 import mysql.connector 
 # Retrieving hidden credentials to establish connection
@@ -29,6 +31,7 @@ else:
 '''
 
 # Creating the Database
+'''
 cursor = connection.cursor()
 cursor.execute("CREATE DATABASE IF NOT EXISTS BoilingPoint_EMS")
 
@@ -36,3 +39,42 @@ cursor.execute("CREATE DATABASE IF NOT EXISTS BoilingPoint_EMS")
 cursor.execute("USE BoilingPoint_EMS")
 cursor.execute("CREATE TABLE Employee_Database (EmpID INT(7) PRIMARY KEY, Name VARCHAR(100), \
     Email TEXT(100), PhoneNum INT(11), Address TEXT(1000), Position TEXT(100), Pay FLOAT(50), TipPerc INT(200))")
+'''
+
+# Creating a menu to interact with the management system
+def menu():
+    system("cls")
+    print("{:>60}".format("===========> Employee Management System <==========="))
+    print("1. Add Employee")
+    print("2. Display Employee Record")
+    print("3. Update Employee Record")
+    print("4. Delete Employee")
+    print("5. Search Employee Database")
+    print("6. Exit\n")
+    print("{:>54}".format("===> Type either: [1/2/3/4/5/6] <==="))
+    
+    ans = input("Enter number: ")
+    if ans == '1':
+        system("cls")
+        # Call add function
+    elif ans == '2':
+        system("cls")
+        # Call display function
+    elif ans == '3':
+        system("cls")
+        # Call update function
+    elif ans == '4':
+        system("cls")
+        # Call delete function
+    elif ans == '5':
+        system("cls")
+        # Call search function
+    elif ans == '6':
+        print("Exiting the program")
+        sys.exit()  # Gracefully exit the program
+    else:
+        print("Unavailable option, try again")
+     
+# Calling menu
+if __name__ == "__main__":
+    menu()
